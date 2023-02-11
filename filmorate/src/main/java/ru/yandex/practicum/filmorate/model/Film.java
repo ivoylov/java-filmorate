321fcf0c6507f1a9f970ea1381dea97711bac389
Vitaliy <vit.ivoylov@yandex.ru>
2023-02-11 22:41:36 +0500

    feat:creat User and Film classes

---
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+@Data
+public class Film {
+
+    Integer id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    Duration duration;
+
+}
