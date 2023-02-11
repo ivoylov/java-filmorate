75fad152883a5c3c9d9b6d305fb4663bb77ac626
Vitaliy <vit.ivoylov@yandex.ru>
2023-02-11 23:47:18 +0500

    feat:add user validate to addUser and updateUser

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.exception.FilmValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 
 @RestController
@@ -12,17 +14,36 @@ public class UserController {
     HashSet<User> users = new HashSet<>();
 
     public User addUser(@RequestBody User user) {
-        if (users.add(user)) return user;
+        if (validateUser(user)) {
+            if (users.add(user)) return user;
+        } else {
+            throw new FilmValidationException();
+        }
         return null;
     }
 
     public User updateUser(@RequestBody User user) {
-        if (users.add(user)) return user;
+        if (validateUser(user)) {
+            if (users.add(user)) return user;
+        } else {
+            throw new FilmValidationException();
+        }
         return null;
     }
 
     public HashSet<User> getAllUsers() {
         return new HashSet<>(users);
+    }
+
+    private boolean validateUser(User user) {
+        if (user.getEmail().isBlank() ||
+            !user.getEmail().contains("@") ||
+            user.getLogin().isBlank() ||
+            user.getLogin().contains(" ") ||
+            user.getBirthday().isAfter(LocalDate.now())
+        ) return false;
+        if (user.getName().isBlank()) user.setName(user.getLogin());
+        return true;
     }
 
 }
