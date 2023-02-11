733c362ef2a6b525719c2a86c0af1095a1f4c9a5
Vitaliy <vit.ivoylov@yandex.ru>
2023-02-11 22:57:27 +0500

    feat:add plug methods to Film class:

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,9 +1,25 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.HashSet;
 
 @RestController
 public class FilmController {
 
+
+    public void addFilm(Film film) {
+
+    }
+
+
+    public void updateFilm(Film film) {
+
+    }
+
+    public HashSet<Film> getAllFilms() {
+        return null;
+    }
     
 }
