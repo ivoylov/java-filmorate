b56c5f45ec19dd7773b3890deee446375cc0c91c
Vitaliy <vit.ivoylov@yandex.ru>
2023-02-11 23:05:39 +0500

    feat:create methods body in FilmContreoller class

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -11,15 +11,15 @@ public class FilmController {
     HashSet<Film> films = new HashSet<>();
 
     public void addFilm(Film film) {
-
+        films.add(film);
     }
 
     public void updateFilm(Film film) {
-
+        films.add(film);
     }
 
     public HashSet<Film> getAllFilms() {
-        return null;
+        return new HashSet<>(films);
     }
 
 }
