3d155d3e91c3d08dc66963472fd407086e333681
Vitaliy <vit.ivoylov@yandex.ru>
2023-02-11 23:35:09 +0500

    feat:add film validate to addFilm and updateFilm

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.exception.FilmValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import java.time.LocalDate;
 import java.util.HashSet;
@@ -9,15 +10,25 @@ import java.util.HashSet;
 @RestController
 public class FilmController {
 
-    HashSet<Film> films = new HashSet<>();
+    private final HashSet<Film> films = new HashSet<>();
+    private final int MAX_LENGTH_FILM_DESCRIPTION = 200;
+    private final LocalDate MINIMAL_RELEASE_DATE = LocalDate.of(1895, 112, 28);
 
     public Film addFilm(@RequestBody Film film) {
-        if (films.add(film)) return film;
+        if (validateFilm(film)) {
+            if (films.add(film)) return film;
+        } else {
+            throw new FilmValidationException();
+        }
         return null;
     }
 
     public Film updateFilm(@RequestBody Film film) {
-        if (films.add(film)) return film;
+        if (validateFilm(film)) {
+            if (films.add(film)) return film;
+        } else {
+            throw new FilmValidationException();
+        }
         return null;
     }
 
@@ -27,8 +38,8 @@ public class FilmController {
 
     private boolean validateFilm(Film film) {
         if (film.getName().isBlank() ||
-            film.getDescription().length() > 200 ||
-            film.getReleaseDate().isBefore(LocalDate.of(1895, 112, 28)) ||
+            film.getDescription().length() > MAX_LENGTH_FILM_DESCRIPTION ||
+            film.getReleaseDate().isBefore(MINIMAL_RELEASE_DATE) ||
             film.getDuration().isZero()) return false;
         return true;
     }
