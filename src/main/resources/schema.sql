cb2446d8a1c4d3a8fb98b6bfb0a92165ccb7e092
Vitaliy <vit.ivoylov@yandex.ru>
2023-04-02 15:56:14 +0500

    feat:add references to dataBase

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,26 @@
+CREATE TABLE IF NOT EXISTS mpa (
+    mpa_id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar
+);
+
+CREATE TABLE IF NOT EXISTS likes (
+     film_id bigint,
+     user_id bigint
+);
+
+CREATE TABLE IF NOT EXISTS genre (
+     genre_id int AUTO_INCREMENT PRIMARY KEY,
+     name varchar
+);
+
 CREATE TABLE IF NOT EXISTS film (
     film_id bigint AUTO_INCREMENT PRIMARY KEY,
     name varchar,
     description varchar,
     release_date date,
     duration bigint,
-    mpa_id int
+    mpa_id int,
+    foreign key (mpa_id) references mpa(mpa_id)
 );
 
 CREATE TABLE IF NOT EXISTS filmorate_user (
@@ -15,25 +31,13 @@ CREATE TABLE IF NOT EXISTS filmorate_user (
     birthdate date
 );
 
-CREATE TABLE IF NOT EXISTS mpa (
-    mpa_id int AUTO_INCREMENT PRIMARY KEY,
-    name varchar
-);
-
-CREATE TABLE IF NOT EXISTS genre (
-    genre_id int AUTO_INCREMENT PRIMARY KEY,
-    name varchar
-);
-
 CREATE TABLE IF NOT EXISTS film_genre (
     genre_id int,
-    film_id int
+    film_id int,
+    foreign key (genre_id) references genre(genre_id)
 );
 
-CREATE TABLE IF NOT EXISTS likes (
-    film_id bigint,
-    user_id bigint
-);
+
 
 CREATE TABLE IF NOT EXISTS relationship_status (
     status_id int AUTO_INCREMENT PRIMARY KEY,
@@ -44,7 +48,10 @@ CREATE TABLE IF NOT EXISTS relationship (
     relation_id bigint AUTO_INCREMENT PRIMARY KEY,
     user_id bigint,
     friend_id bigint,
-    status_id int
+    status_id int,
+    foreign key (user_id) references filmorate_user(user_id),
+    foreign key (friend_id) references filmorate_user(user_id),
+    foreign key (status_id) references relationship_status (status_id)
 );
 
 
